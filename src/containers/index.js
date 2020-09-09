b28966df10ea6451c60554a9caa921a172d27270
spk-cpsamut <taresupakon@gmail.com>
2020-09-09 12:39:44 +0700

    [FIXED] fixed templete core-ui , give it, can compile

---
+++ src/containers/index.js
@@ -0,0 +1,21 @@
+import TheContent from "./Thecontent";
+import TheFooter from "./Thefooter";
+import TheHeader from "./Theheader";
+import TheHeaderDropdown from "./Theheaderdropdown";
+import TheHeaderDropdownMssg from "./Theheaderdropdownmssg";
+import TheHeaderDropdownNotif from "./Theheaderdropdownnotif";
+import TheHeaderDropdownTasks from "./Theheaderdropdowntasks";
+import TheLayout from "./Thelayout";
+import TheSidebar from "./Thesidebar";
+
+export {
+  TheContent,
+  TheFooter,
+  TheHeader,
+  TheHeaderDropdown,
+  TheHeaderDropdownMssg,
+  TheHeaderDropdownNotif,
+  TheHeaderDropdownTasks,
+  TheLayout,
+  TheSidebar,
+};
