3f3838b1058ab023b532725886ed93df3957b379
spk-cpsamut <taresupakon@gmail.com>
2020-09-09 13:00:11 +0700

    [UPDATE] add config folder push config role file

--- src/config/role.js
+++ src/config/role.js
@@ -1,1 +1,41 @@
+import React from "react";
 
+const Login = React.lazy(() => import("../views/pages/login/Login"));
+const Register = React.lazy(() => import("../views/pages/register/Register"));
+const Page404 = React.lazy(() => import("../views/pages/page404/Page404"));
+const Page500 = React.lazy(() => import("../views/pages/page500/Page500"));
+const TheLayOut = React.lazy(() => import("../containers/TheLayout"));
+
+const components = {
+  login: {
+    component: Login,
+    url: "/login",
+  },
+  register: {
+    component: Register,
+    url: "/register",
+  },
+  page404: {
+    component: Page404,
+    url: "/page404",
+  },
+  page500: {
+    component: Page500,
+    url: "/page500",
+  },
+  home: {
+    component: TheLayOut,
+    url: "/home",
+  },
+};
+
+const allowRoute = {
+  guest: {
+    allow: [components.login],
+    redirect: "/login",
+  },
+  member: {
+    allow: [components.home, components.page404, components.page500],
+    redirect: "/home",
+  },
+};
